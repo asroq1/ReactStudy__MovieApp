709afc983692acda9b054f2aa9b102a0cef00d7d
marsoon <asroq98@gmail.com>
2021-02-01 18:52:28 +0900

    Add movie title

---
+++ src/components/movie.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from "prop-types";
+
+const Movie = ({id, year ,title, summary, poster }) => {
+    return (
+        <h5>{title}</h5>
+    );
+};
+
+Movie.prototype={
+    id : PropTypes.number.isRequired,
+    year : PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    summary : PropTypes.string.isRequired,
+    poster : PropTypes.string.isRequired
+}
+export default Movie;
